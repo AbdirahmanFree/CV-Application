8e8a4765163ffec3b1ae0bf824b1ba22c38dc3dc
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-18 12:43:10 -0500

    created hook for app allows user to enter information and store it betweeen renders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,54 @@ import '../styles/App.css'
 import {Header} from './Header.jsx'
 
 function App() {
+  const [cvData, setCvData] = useState({
+    'personal': {
+      'name': '',
+      'number': '',
+      'email': '',
+      'linkedIn': '',
+      'gitHub': '',
+    },
+    'education': [
+      {
+        'id': crypto.randomUUID(),
+        'school': '',
+        'location': '',
+        'degree': '',
+        'start': '',
+        'end': '',
+        'bullets': [],
+      },
+    ],
+    'experience': [
+      {
+        'id': crypto.randomUUID(),
+        'title': '',
+        'start': '',
+        'end': '',
+        'company': '',
+        'location': '',
+        'bullets': [],
+      },
+    ],
+    'projects': [
+      {
+        'id': crypto.randomUUID(),
+        'title': '',
+        'technologies': '',
+        'bullets': [],
+      },
+    ],
+    'skills': [
+      {
+        'id': crypto.randomUUID(),
+        'skillGroup': '',
+        'skills': '',
+      },
+    ],
+
   
+  })
 
   return (
     <div className='App'>
