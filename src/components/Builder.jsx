1862886e6261685be7816aaa962cb47f3a3db939
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-19 17:30:12 -0500

    builder

--- src/components/Builder.jsx
+++ src/components/Builder.jsx
@@ -3,14 +3,11 @@ import '../styles/Builder.css'
 function Builder({cvData, setCvData}){
     return (
         <div className='content'>
-            <Section personal={cvData.personal} setCvData />
-            <Section personal={cvData.education} setCvData />
-            <Section personal={cvData.experience} setCvData />
-            <Section personal={cvData.projects} setCvData />
-            <Section personal={cvData.skills} setCvData />
-            
-            
-
+            <Section personal={cvData.personal} setCvData={setCvData} />
+            <Section personal={cvData.education} setCvData={setCvData} />
+            <Section personal={cvData.experience} setCvData={setCvData} />
+            <Section personal={cvData.projects} setCvData={setCvData} />
+            <Section personal={cvData.skills} setCvData={setCvData} />
         </div>
     )
 }
