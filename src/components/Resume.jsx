06176aad12422197c5413843d1cf3a88dbb150c2
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-19 17:45:01 -0500

    gonna build resume section components later

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,11 +3,6 @@ import '../styles/Resume.css'
 function Resume({cvData}){
     return (
         <div className='content'>
-            <ResumeSection personal={cvData.personal} />
-            <ResumeSection education={cvData.education} />
-            <ResumeSection experience={cvData.experience} />
-            <ResumeSection projects={cvData.projects} />
-            <ResumeSection skills={cvData.skills} />
         </div>
     )
 }
