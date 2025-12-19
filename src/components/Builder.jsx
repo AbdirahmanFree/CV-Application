7a0dfd68fee31a9a105391853a877ace795ba481
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-19 18:00:48 -0500

    lowkey vibecoded this buit started with the structure of builder section

--- src/components/Builder.jsx
+++ src/components/Builder.jsx
@@ -1,13 +1,14 @@
 import '../styles/Builder.css'
+import { BuilderSection } from './BuilderSection'
 
 function Builder({cvData, setCvData}){
     return (
         <div className='content'>
-            <BuilderSection personal={cvData.personal} setCvData={setCvData} />
-            <BuilderSection education={cvData.education} setCvData={setCvData} />
-            <BuilderSection experience={cvData.experience} setCvData={setCvData} />
-            <BuilderSection projects={cvData.projects} setCvData={setCvData} />
-            <BuilderSection skills={cvData.skills} setCvData={setCvData} />
+            <BuilderSection section="personal" sectionData={cvData.personal} setCvData={setCvData} />
+            <BuilderSection section="education" sectionData={cvData.education} setCvData={setCvData} />
+            <BuilderSection section="experience" sectionData={cvData.experience} setCvData={setCvData} />
+            <BuilderSection section="projects" sectionData={cvData.projects} setCvData={setCvData} />
+            <BuilderSection section="skills" sectionData={cvData.skills} setCvData={setCvData} />
         </div>
     )
 }
