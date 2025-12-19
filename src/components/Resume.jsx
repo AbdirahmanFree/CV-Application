6906ae460e6171a753412a68a74ddc65fdb0191b
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-19 17:34:44 -0500

    completed structure of resume component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,14 @@
 import '../styles/Resume.css'
 
-function Resume(){
+function Resume({cvData}){
     return (
-        <>
-        </>
+        <div>
+            <Section personal={cvData.personal} />
+            <Section personal={cvData.education} />
+            <Section personal={cvData.experience} />
+            <Section personal={cvData.projects} />
+            <Section personal={cvData.skills} />
+        </div>
     )
 }
 
