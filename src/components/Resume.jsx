c96f3bc413f9eb31ef58d026f9cbf2ad41986e02
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-19 17:43:16 -0500

    changed resumesection component name

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,11 +3,11 @@ import '../styles/Resume.css'
 function Resume({cvData}){
     return (
         <div className='content'>
-            <Section personal={cvData.personal} />
-            <Section education={cvData.education} />
-            <Section experience={cvData.experience} />
-            <Section projects={cvData.projects} />
-            <Section skills={cvData.skills} />
+            <ResumeSection personal={cvData.personal} />
+            <ResumeSection education={cvData.education} />
+            <ResumeSection experience={cvData.experience} />
+            <ResumeSection projects={cvData.projects} />
+            <ResumeSection skills={cvData.skills} />
         </div>
     )
 }
