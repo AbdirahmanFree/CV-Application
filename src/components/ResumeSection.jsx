aab0940fdc6cd9e9a7c563039016090109a8689e
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-22 11:11:48 -0500

    added personal resume section allowing users to see what they saved

--- src/components/ResumeSection.jsx
+++ src/components/ResumeSection.jsx
@@ -3,7 +3,7 @@ import '../styles/ResumeSection.css'
 function ResumeSection({ section, sectionData}){
     if (section == "personal"){
         return (
-            <div>
+            <div className='main'>
                 <h1>{sectionData.name}</h1>
             </div>
         )
