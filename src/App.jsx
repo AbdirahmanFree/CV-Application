1035295e6819df42fb4175653579ca533ac4edb3
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-23 20:54:11 -0500

    started designing bullets

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [cvData, setCvData] = useState({
     'personal': {
       'name': 'Freddie Kruger',
-      'number': '012-345-798',
+      'number': '012-345-6798',
       'email': 'readyfreddie@gmail.com',
       'linkedIn': 'https://www.linkedin.com/in/freddie-kruger/',
       'portfolio': 'https://github.com/freddie-kruger',
@@ -35,13 +35,57 @@ function App() {
     'experience': [
       {
         'id': crypto.randomUUID(),
-        'title': '',
-        'start': '',
-        'end': '',
-        'company': '',
-        'location': '',
-        'bullets': [],
+        'title': 'Field Analyst - Criminal Patterns',
+        'start': 'Sept 1978',
+        'end': 'Jun 1984',
+        'company': 'Springwood Independent Research',
+        'location': 'Springwood, Ohio',
+        'bullets': [
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Studied behavioral patterns of local criminal activity for case analysis.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Developed expertise in nocturnal surveillance and evasion tactics.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Documented methodologies of high-risk individuals for theoretical research purposes.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Analyzed crime scene dynamics to understand psychological triggers and patterns.'
+          }
+        ],
       },
+      {
+        'id': crypto.randomUUID(),
+        'title': 'Night Operations Research Assistant',
+        'start': 'Jul 1984',
+        'end': 'Dec 1989',
+        'company': 'Dreamscape Behavioral Lab',
+        'location': 'Springwood, Ohio',
+        'bullets': [
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Monitored and recorded nocturnal activity for patterns in sleep behavior and subconscious responses.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Assisted in the development of theoretical models for fear conditioning and nightmare induction.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Conducted field observations to correlate environmental factors with high-risk behavioral tendencies.'
+          },
+          {
+            'id': crypto.randomUUID(),
+            'text': 'Prepared detailed reports and visual analyses for senior researchers on anomalous activity trends.'
+          }
+        ],
+      }
+
     ],
     'projects': [
       {
--- src/components/BuilderSection.jsx
+++ src/components/BuilderSection.jsx
@@ -3,7 +3,7 @@ import "../styles/BuilderSection.css"
 
 function BuilderSection({ section, sectionData, setCvData}){
     const [bullets, setBullets] = useState(sectionData.bullets)
-    const [sections, setSections] = useState([''])
+    const [sections, setSections] = useState(sectionData)
     if(section ==="personal"){
         function handleSubmit(e){
             e.preventDefault()
@@ -143,10 +143,77 @@ function BuilderSection({ section, sectionData, setCvData}){
         )
     }
     if(section ==="experience"){
+        function handleSubmit(e){
+            e.preventDefault()
+            const form = e.currentTarget
+
+        }
+        function handleBulletChange(id, value){
+            setBullets(prevBullets => 
+                prevBullets.map(b => (b.id == id ? {...b, 'text': value} : b))
+            )
+
+        }
+        function addBullet(){
+            setBullets(prevBullets => [...prevBullets, {'id': crypto.randomUUID(), 'text': ''}])
+        }
+
+        function removeBullet(id){
+            setBullets(prev => prev.filter(b => b.id != id))
+        }
         return (
-        <>
-        </>
+        <div className="sections">
+            {sectionData.map(experience => {
+                return (
+                    <form className="section" onSubmit={handleSubmit}>
+                        <div className="field">
+                            <label htmlFor={`title-${experience.id}`} >Title:</label>
+                            <input id={`title-${experience.id}`} name="title" value={experience.title} />
+                        </div>
+                        <div className="field">
+                            <label htmlFor={`start-${experience.id}`} >Start:</label>
+                            <input id={`start-${experience.id}`} name="start" value={experience.start} />
+                        </div>
+                        <div className="field">
+                            <label htmlFor={`end-${experience.id}`} >End:</label>
+                            <input id={`end-${experience.id}`} name="end" value={experience.end} />
+                        </div>
+                        <div className="field">
+                            <label htmlFor={`company-${experience.id}`} >Company:</label>
+                            <input id={`company-${experience.id}`} name="company" value={experience.company} />
+                        </div>
+                        <div className="field">
+                            <label htmlFor={`location-${experience.id}`} >location:</label>
+                            <input id={`location-${experience.id}`} name="location" value={experience.location}/>
+                        </div>
+                        <div className="bullets">
+                            {experience.bullets.map(bullet => {
+                                return (
+                                    <div className="field">
+                                        <label htmlFor={bullet.id}>Bullet:</label>
+                                        <input id={bullet.id} name={bullet.id} value={bullet.text}
+                                        onChange={(e) => handleBulletChange(bullet.id, e.target.value)}
+                                        />
+                                        <button type= "button" onClick={() => removeBullet(bullet.id)}>X</button>
+                                    </div>
+                                )
+                            })}
+                            <button  type="button" onClick={addBullet}>Add</button>
+
+                        </div>
+
+
+                        <div>
+                            <button type="submit"> Save</button>
+                            <button onClick={(e) => deleteSection(experience.id)}>Delete</button>
+                        </div>
+                    </form>
+                )
+            })}
+        </div>
         )
+        
+        
     }
     if(section ==="projects"){
         return (
