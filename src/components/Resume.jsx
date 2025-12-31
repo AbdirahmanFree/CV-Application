90c322dfecca18899e14e0aba9e58318dfa00354
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-31 18:38:56 -0500

    Finished Experience section on resume changed font for ATS

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,14 @@
 import '../styles/Resume.css'
-import { ResumeSection } from './ResumeComponents/ResumeSection'
 import { PersonalSection } from './ResumeComponents/PersonalSection'
 import { EducationSection } from './ResumeComponents/EducationSection'
+import { ExperienceSection } from './ResumeComponents/ExperienceSection'
 
 function Resume({cvData}){
     return (
         <div className='resume-container'>
             <PersonalSection sectionData={cvData.personal} />
             <EducationSection sectionData={cvData.education} />
+            <ExperienceSection sectionData={cvData.experience} />
         </div>
     )
 }
--- src/components/ResumeComponents/ExperienceSection.jsx
+++ src/components/ResumeComponents/ExperienceSection.jsx
@@ -1,1 +1,35 @@
 import '../../styles/ResumeSection.css'
+
+function ExperienceSection({sectionData}){
+    return (
+        <div className='experience container'>
+            <div className='heading'>Experience</div>
+            <div className='section-content'>
+                {sectionData.map(experience => (
+                    <div>
+                        <div className='section-field'>
+                            <span className='bold'>{experience.title}</span>
+                            <span className='italic'>{experience.start} - {experience.end}</span>
+                        </div>
+                        <div className='section-field'>
+                            <span className='italic'>{experience.company}</span>
+                            <span className='italic'>{experience.location}</span>
+                        </div>
+
+                        <div className='section-field'>
+                            <ul>
+                                {experience.bullets.map(bullet => (
+                                    <li>{bullet.text}</li>
+                                ))}
+                            </ul>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+export {ExperienceSection}
