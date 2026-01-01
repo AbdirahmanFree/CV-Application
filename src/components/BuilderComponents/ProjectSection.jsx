23cdcaf130a4cba0d8a7353131ec1a3211d0fc4d
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-12-31 19:26:40 -0500

    started on Project section

---
+++ src/components/BuilderComponents/ProjectSection.jsx
@@ -0,0 +1,12 @@
+import '../../styles/BuilderSection.css'
+import { useState } from 'react'
+
+function ExperienceSection({sectionData, setCvData}){
+    const [bullets, setBullets] = useState(sectionData.bullets)
+
+    return (
+        <form className='projects'>
+
+        </form>
+    )
+}
